230c47de72bc64a0188d4fa8b893afe37cf151f5
siwaht <siwaht.com@gmail.com>
2026-01-14 14:46:59 +0530

    Fix: Comment out missing @picahq/toolkit import in pica-toolkit.ts

--- server/services/pica-toolkit.ts
+++ server/services/pica-toolkit.ts
@@ -1,16 +1,16 @@
-import { Pica } from "@picahq/toolkit";
+// import { Pica } from "@picahq/toolkit";
 
 export class PicaToolkitService {
     private pica: Pica;
 
     constructor(apiKey: string) {
-        this.pica = new Pica(apiKey, {
-            connectors: ["*"],
-            actions: ["*"]
-        });
+        // this.pica = new Pica(apiKey, {
+//             connectors: ["*"],
+//             actions: ["*"]
+//         });
     }
 
     get instance(): Pica {
-        return this.pica;
+        // return this.pica;
     }
 }
